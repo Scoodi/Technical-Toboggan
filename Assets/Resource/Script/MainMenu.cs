c120fd1ccfdf4f6b6bae1c2cb9c6487ce815982f
Lewis-S <45666485+Lewis-S@users.noreply.github.com>
2020-02-18 10:59:18 +0000

    Implemented a Main Menu
    Created a basic main menu based on the menu flowchart. This menu is able to load the next scene. The functionality of the menu (displaying scores and changing settings) is not yet implemented

---
+++ Assets/Resource/Script/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+    //Change to the character selection scene 
+    public void CharacterSelect ()
+    {
+        SceneManager.LoadScene("CharacterSelection");
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+  
+}
